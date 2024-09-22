229173f193b53e0b8a3236b987bd07af460a7613
ciaran <ciaranb333@gmail.com>
2024-09-22 22:07:59 +0100

    added pagination to recipes list

--- recipe_room/recipes/views.py
+++ recipe_room/recipes/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Recipe
 
@@ -5,8 +6,13 @@ def home(request):
     return render(request, 'recipes/home.html')
 
 def recipe_list(request):
-    recipes = Recipe.objects.all()
-    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+    recipe_list = Recipe.objects.all()
+    paginator = Paginator(recipe_list, 4)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'recipes/recipe_list.html', {'page_obj': page_obj})
 
 def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
