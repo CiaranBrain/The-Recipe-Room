fa2c4c0b6a807a11e90e5626691b39258cb18613
ciaran <ciaranb333@gmail.com>
2024-09-22 21:15:50 +0100

    Added recipe app, model, urls, views, templates

--- recipe_room/recipes/models.py
+++ recipe_room/recipes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    """Represents a recipe with detailed information and an image."""
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- recipe_room/recipes/views.py
+++ recipe_room/recipes/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
 
-# Create your views here.
+def home(request):
+    return render(request, 'recipes/home.html')
+
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
