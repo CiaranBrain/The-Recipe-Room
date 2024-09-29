baa897c61e084c842f0413fb72c9d4dc97dc505e
ciaran <ciaranb333@gmail.com>
2024-09-29 16:00:30 +0100

    restructed html styling, in most htmls for better styling with bootstrap

--- recipe_room/account/forms.py
+++ recipe_room/account/forms.py
@@ -4,22 +4,33 @@ from django.contrib.auth.models import User
 class LoginForm(forms.Form):
     username = forms.CharField(
         label='Username',
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your username'})
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your username',
+            'aria-label': 'Username'  # For accessibility
+        })
     )
     password = forms.CharField(
         label='Password',
-        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter your password'})
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your password',
+            'aria-label': 'Password'  # For accessibility
+        })
     )
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password',
-                                widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password',
-                                widget=forms.PasswordInput)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter your password'}))
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Repeat your password'}))
 
     class Meta:
         model = User
         fields = ['username', 'first_name', 'email']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your username'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your first name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email address'}),
+        }
 
     def clean_password2(self):
         cd = self.cleaned_data
